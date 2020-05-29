8825981fcc3da662ec1521ad3bf1406e0b2d8a37
DOINE Jacquenson <doinejacquenson@gmail.com>
2020-05-29 07:24:07 -0400

    Fill the posts fragment with item

--- app/src/main/java/fragments/PostsFragment.java
+++ app/src/main/java/fragments/PostsFragment.java
@@ -2,23 +2,33 @@ package fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.instagram.Post;
 import com.example.instagram.R;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class PostsFragment extends Fragment {
 
+    private RecyclerView rvPosts;
     public PostsFragment() {
         // Required empty public constructor
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -26,4 +36,37 @@ public class PostsFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_posts, container, false);
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState){
+        super.onViewCreated(view, savedInstanceState);
+        rvPosts = view.findViewById(R.id.rvPosts);
+
+        // Steps to use the recycler view:
+        //0. Create layout for one row in the list
+        //1. create the adapter
+        //2.create the data source
+        //3.set the adapter on the recycler view
+        //4.set the manager on the recycler view
+    }
+
+    private void queryPosts() {
+        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
+        query.include(Post.KEY_USER);
+
+        query.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> posts, ParseException e) {
+                if (e != null){
+                    Log.e("TAG", "Issue with getting posts", e);
+                    return;
+                }
+
+                for(Post post : posts){
+                    Log.i("TAG", "Post: " + post.getDescription() + ", username: " + post.getUser().getUsername());
+                }
+            }
+        });
+    }
+
 }
